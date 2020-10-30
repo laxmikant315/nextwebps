a27d2b52c25e7ba42eef541254eba1ffcf868d6d
Laxmikant Phadke <laxmikantphadke@gmail.com>
2020-10-30 17:33:14 +0530

    Add files via upload

---
+++ precache-manifest.ef8f013fec56dcfd5106082c8c8fdddf.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "303b47c9b142f6c0d4c6",
+    "url": "/nextwebps/static/js/app.230bb6d2.chunk.js"
+  },
+  {
+    "revision": "691fba0da9ee1ab06f27",
+    "url": "/nextwebps/static/js/runtime~app.d71b2fe3.js"
+  },
+  {
+    "revision": "26be57f35337bf9a9da8",
+    "url": "/nextwebps/static/js/2.15ffec62.chunk.js"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/nextwebps/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/nextwebps/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "ca9ce9ff0676a9b04ef0f8a3ad17dd08",
+    "url": "/nextwebps/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/nextwebps/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "b49ae8ab2dbccb02c4d11caaacf09eab",
+    "url": "/nextwebps/./fonts/Fontisto.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/nextwebps/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/nextwebps/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "3c851d60ad5ef3f2fe43ebd263490d78",
+    "url": "/nextwebps/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/nextwebps/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/nextwebps/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "b70cea0339374107969eb53e5b1f603f",
+    "url": "/nextwebps/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "c39278f7abfc798a241551194f55e29f",
+    "url": "/nextwebps/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "54a91b0619ccf9373d525109268219dc",
+    "url": "/nextwebps/./fonts/Roboto.ttf"
+  },
+  {
+    "revision": "99fc0816a09395454061301fefa42bf1",
+    "url": "/nextwebps/./fonts/Roboto_medium.ttf"
+  },
+  {
+    "revision": "06984d029b2edd11080008d78f31af56",
+    "url": "/nextwebps/index.html"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/nextwebps/serve.json"
+  },
+  {
+    "revision": "2002987acac9deb6803ab4ba381226ba",
+    "url": "/nextwebps/expo-service-worker.js"
+  },
+  {
+    "revision": "5456d129834994fc723fa55c73ad5ca1",
+    "url": "/nextwebps/register-service-worker.js"
+  },
+  {
+    "revision": "4d30230f5fb873588451568bd54b1165",
+    "url": "/nextwebps/static/js/2.15ffec62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a64c5c726c073d0f83185f9dafb92a12",
+    "url": "/nextwebps/favicon-16.png"
+  },
+  {
+    "revision": "24ed4672f019214d8a34e3860343cb88",
+    "url": "/nextwebps/favicon-32.png"
+  },
+  {
+    "revision": "192b18d0ad6054e7f1db05f4117f5dba",
+    "url": "/nextwebps/favicon.ico"
+  },
+  {
+    "revision": "0fe3cdc0cb1e03837acd75ce1d1f4f0f",
+    "url": "/nextwebps/manifest.json"
+  }
+];
---
+++ register-service-worker.js
@@ -0,0 +1,1 @@
+"serviceWorker"in navigator&&window.addEventListener("load",(function(){navigator.serviceWorker.register("/nextwebps/expo-service-worker.js",{scope:"/nextwebps/"}).then((function(e){})).catch((function(e){console.info("Failed to register service-worker",e)}))}));
